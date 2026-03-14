e557b8c413dbe04bd96fa11e787811215de42481
Dardan Demiri <dardantheman@gmail.com>
2026-03-14 17:38:11 -0400

    fix: update image URL builder import and type definitions

--- frontend/src/lib/sanity/image.ts
+++ frontend/src/lib/sanity/image.ts
@@ -1,8 +1,11 @@
-import { createImageUrlBuilder } from "@sanity/image-url";
-import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
+import imageUrlBuilder, {
+  type ImageUrlBuilder,
+} from "@sanity/image-url";
 import { client } from "./client";
 
-const builder = createImageUrlBuilder(client);
+type SanityImageSource = Parameters<ImageUrlBuilder["image"]>[0];
+
+const builder = imageUrlBuilder(client);
 
 export function urlFor(source: SanityImageSource) {
   return builder.image(source);
